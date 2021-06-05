6332199ec14547ce9b79eb3e10d836ff8c2acd80
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-06-05 16:00:00 +0100

    Initial Commit

---
+++ Rough/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rough
+//
+//  Created by nurudeen on 05/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rough/RoughApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoughApp.swift
+//  Rough
+//
+//  Created by nurudeen on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct RoughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
